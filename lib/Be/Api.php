9989e5bf6ce38990ffd32d2785c54bd5a33037eb
Bryan Latten <bryan@behance.com>
2012-09-04 19:34:41 -0400

    Be_Api: added method to retrieve a user's appreciated projects

--- lib/Be/Api.php
+++ lib/Be/Api.php
@@ -121,6 +121,27 @@ class Be_Api {
 
 
   /**
+   * Retrieves a list of projects that $id_or_username has appreciated
+   *
+   * @param int|string $id_or_username : user's projects to search
+   * @param bool       $assoc          : return objects will be converted to associative arrays
+   *
+   * @return array : stdClass objects or associative arrays, based on $assoc
+   */
+  public function getUserAppreciations( $id_or_username, $assoc = false ) {
+
+    $endpoint = static::ENDPOINT_USERS . '/' . $id_or_username . '/appreciations';
+    $results  = $this->_getDecodedJson( $endpoint, array(), 'appreciations', $assoc );
+
+    // IMPORTANT: Ensure this will always return an array
+    return ( empty( $results ) )
+           ? array()
+           : $results;
+
+  } // getUserAppreciations
+
+
+  /**
    * Retrieves a list of $id_or_username's works in progress
    *
    * @param int|string $id_or_username : user's works in progress to search
